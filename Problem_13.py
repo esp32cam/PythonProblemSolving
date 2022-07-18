54bce84a797a9bc403304f36316562498bdd8a00
Pongsapak Khongchae <56827580+esp32cam@users.noreply.github.com>
2022-07-18 23:57:18 +0700

    Add files via upload

---
+++ Problem_13.py
@@ -0,0 +1,15 @@
+
+def hahaha(x):
+    count_5 = 0
+    for i in range(len(x)):
+        if x[i] == "5":
+            count_5 += 1
+    if "555" not in x:
+        print("none")
+    elif "555" in x and count_5 == 3:
+        print("hahaha")
+    elif count_5 != 3:
+        print("none")
+
+n = input().__str__()
+hahaha(n)
---
+++ Problem_14.py
@@ -0,0 +1,17 @@
+'''A secret door in a castle It can be opened using a 5-position code,
+each of which can be a combination of numbers 0-9 a-z and A-Z. The castle
+keeper knows only the first four codes, but the last one is missing.
+He knows the last one. There is a method of calculating using the data
+from the first 4 characters – if the first code is a number. The last
+digit will also be a number. where the last digit is the first numeric
+value plus 9 then mod with 5 – if the first letter is a lowercase letter
+The last digit will be a number. What will the last code be? Consider
+how many digits are the next 3 letters. Take a number that is a positive
+number 2 and mod it with 3 – but if the first code is a capital letter
+The last digit will be the same character as the first code. but whether
+it is uppercase or lowercase, determine whether the last preceding code
+(4th digit code) is uppercase or lowercase. if it is uppercase or numeric
+The last code will be a lowercase letter. But if the code before the last
+is lowercase The last code will be uppercase. Write a program to help find
+the missing location code.
+Then type the complete code out on the screen.'''
